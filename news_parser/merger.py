959cbb5b61b4807654cb6e97accbbabec6fd434b
pazamelin <pazamelin@edu.hse.ru>
2022-02-01 00:12:50 +0300

    Update merger, merge files

--- news_parser/merger.py
+++ news_parser/merger.py
@@ -7,13 +7,19 @@ import pathlib
 
 
 def merge_json_files(path, filenames):
-	result = list()
-	for fn in filenames:
-		with open(f'{path}/{fn}', 'r') as infile:
-			result.extend(json.load(infile))
+	all_articles_list = []
 
+	for fn in filenames:
+		full_fn = f'{path}/{fn}'
+		print(full_fn)
+		with open(full_fn) as json_file:
+			data = json.load(json_file)
+			all_articles_list += data['catalog']
+
+	all_articles_dict = {'catalog': all_articles_list}
 	with open(f'{path}/merged.json', 'w') as output_file:
-		json.dump(result, output_file)
+		json.dump(all_articles_dict, output_file, indent=2)
+
 
 def main():
 	# collect files
@@ -21,7 +27,7 @@ def main():
 	artifacts_path = f'{my_path}/artifacts/'
 	files = [f for f in listdir(artifacts_path) if isfile(join(artifacts_path, f))]
 	files = sorted(files)
-
+	
 	# merge
 	merge_json_files(artifacts_path, files)
 
